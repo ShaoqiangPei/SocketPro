3241d5104b0854172fcdf59d322172f7ee3d2ed6
Pain <769936726@qq.com>
2020-04-30 11:36:43 +0800

    1. 提交socket 库初始版本

---
+++ SocketPro/app/src/main/java/com/socketpro/MainActivity.java
@@ -0,0 +1,33 @@
+package com.socketpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        initData();
+        setListener();
+    }
+
+    /**初始化控件**/
+    private void initView() {
+        initView();
+    }
+
+    /**初始化数据**/
+    private void initData() {
+
+    }
+
+    /**设置监听**/
+    private void setListener() {
+
+    }
+
+}

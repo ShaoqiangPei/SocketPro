3b75c1c8e71bc0c7fa94a653f18759f7f7b1f1bd
Pain <769936726@qq.com>
2020-06-22 11:15:21 +0800

    1. 修改log打印

--- SocketPro/socketlibrary/src/main/java/com/socketlibrary/mina_tcp/tm_connect/TmClientHandler.java
+++ SocketPro/socketlibrary/src/main/java/com/socketlibrary/mina_tcp/tm_connect/TmClientHandler.java
@@ -1,6 +1,6 @@
 package com.socketlibrary.mina_tcp.tm_connect;
 
-import com.util.LogUtil;
+import com.socketlibrary.util.SocketUtil;
 
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IdleStatus;
@@ -30,46 +30,46 @@ public class TmClientHandler extends IoHandlerAdapter {
         super.sessionOpened(session);
         //保存sesion
         SessionManager.getInstance().setSession(session);
-        LogUtil.i("=========mina_tcp客户端连接成功========");
+        SocketUtil.i("=========mina_tcp客户端连接成功========");
     }
 
     @Override
     public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
         super.exceptionCaught(session, cause);
         //客户端连接异常
-        LogUtil.e("=========mina_tcp客户端连接异常========"+cause.getMessage());
-        LogUtil.e("********连接失败可能原因****************");
-        LogUtil.e("1.mina_tcp客户端与服务端链接地址(ip)不一致");
-        LogUtil.e("2.mina_tcp客户端与服务端链接端口(port)不一致");
-        LogUtil.e("3.mina_tcp服务端未开启");
-        LogUtil.e("4.mina_tcp客户端未联网或未开联网权限");
-        LogUtil.e("5.mina_tcp客户端与服务端不在一个网段");
+        SocketUtil.e("=========mina_tcp客户端连接异常========"+cause.getMessage());
+        SocketUtil.e("********连接失败可能原因****************");
+        SocketUtil.e("1.mina_tcp客户端与服务端链接地址(ip)不一致");
+        SocketUtil.e("2.mina_tcp客户端与服务端链接端口(port)不一致");
+        SocketUtil.e("3.mina_tcp服务端未开启");
+        SocketUtil.e("4.mina_tcp客户端未联网或未开联网权限");
+        SocketUtil.e("5.mina_tcp客户端与服务端不在一个网段");
     }
 
     @Override
     public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
         super.sessionIdle(session, status);
-        LogUtil.i("=========sessionIdle=====session="+session+"   IdleStatus="+status);
+        SocketUtil.i("=========sessionIdle=====session="+session+"   IdleStatus="+status);
     }
 
     @Override
     public void sessionClosed(IoSession session) throws Exception {
         super.sessionClosed(session);
 
-        LogUtil.i("=========sessionClosed=====session="+session);
+        SocketUtil.i("=========sessionClosed=====session="+session);
     }
 
     @Override
     public void messageSent(IoSession session, Object message) throws Exception {
         super.messageSent(session, message);
-        LogUtil.i("mina_tcp客户端发送消息："+message.toString());
+        SocketUtil.i("mina_tcp客户端发送消息："+message.toString());
     }
 
     @Override
     public void messageReceived(IoSession session, Object message) throws Exception {
         super.messageReceived(session, message);
 
-        LogUtil.i("mina_tcp客户端收到消息："+message.toString());
+        SocketUtil.i("mina_tcp客户端收到消息："+message.toString());
         if(mOnMrListener!=null){
             mOnMrListener.messageReceived(session,message);
         }
